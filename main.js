cb76253f07c6c0c62116efb12d2d09bc3570eb14
Jorman Urbina <jormanurbina1@gmail.com>
2024-06-04 14:28:26 +0200

    Mejoras en la interfaz y funcionalidad del Comparador de Países:
    Énfasis en el título: Se actualizó el título de la aplicación usando la etiqueta <strong> para resaltar su importancia.
    Símbolo de Moneda en la primera columna:
    Se agregó una clase "simbolo" a la primera columna de la tabla para mostrar el símbolo de la moneda.
    Se modificó la función renderizarPaises para asignar la clase "simbolo" dinámicamente en base a la información de la moneda.
    Optimización de la carga de datos:
    Se implementó la función fetchPaises para realizar la petición al fichero "paises.json" de forma separada.
    Se modificó la función cargarDatos para utilizar la función fetchPaises y filtrar los países con información de moneda.
    Actualización de tarifas:
    Se implementó la función fetchTarifas para realizar la petición a la API externa de conversión de monedas.
    Se actualizó la función actualizarMontos para utilizar la función fetchTarifas y obtener las tasas de conversión actualizadas.
    Selección de países optimizada:
    Se agregaron las propiedades id y classList a las opciones del selector de países para facilitar su manipulación.
    Se actualizó la función renderizarOpcionesDePaisesSeleccionables para agregar la clase del símbolo monetario a cada opción.
    Código mejorado:
    Se refactorizó el código para mejorar la legibilidad y mantenibilidad.
    Se agregaron comentarios explicativos para facilitar la comprensión del código.

--- main.js
+++ main.js
@@ -3,74 +3,95 @@ const seleccionarPais = document.querySelector("#seleccionarPais");
 const tbody = document.querySelector("tbody");
 const template = document.querySelector("template").content;
 const fragment = document.createDocumentFragment();
-let tarifaActual = "EUR";
-let montoActual = 1
-
+const simboloInput = document.querySelector("#inputGroup-sizing-md");
 document.addEventListener("DOMContentLoaded", () => {
   cargarDatos();
 });
+
+const simboloInputInicial = "€";
+const monedaInputInicial = "Euro";
+
+let paisesConMoneda = [];
+let tarifaActual = "EUR";
+let montoActual = 1;
+
+inputMonto.placeholder = `Escribir monto en ${monedaInputInicial}`;
+simboloInput.textContent = simboloInputInicial;
 
 inputMonto.addEventListener("input", (e) => {
   montoActual = Number(e.target.value);
   actualizarMontos(tarifaActual, montoActual);
 });
-seleccionarPais.addEventListener("change", (e) => {
-  actualizarMontos(e.target.value,montoActual);
-});
 
-async function actualizarMontos(tarifaSeleccionada, montoInput) {
-  tarifaActual = tarifaSeleccionada
+seleccionarPais.addEventListener("change", (e) => {
+  const moneda = seleccionarPais.selectedOptions[0].id;
+  tarifaActual = e.target.value;
+  simboloInput.textContent = seleccionarPais.selectedOptions[0].className;
+  inputMonto.placeholder = `Escribir monto en ${moneda}`;
+
+  actualizarMontos(tarifaActual, montoActual);
+});
+async function cargarDatos() {
+  const data = await fetchPaises();
+  paisesConMoneda = data.filter((pais) => pais.currencies);
+  renderizarPaises(paisesConMoneda);
+  renderizarOpcionesDePaisesSeleccionables(paisesConMoneda);
+  actualizarMontos(tarifaActual);
+}
+
+async function fetchPaises() {
+  const response = await fetch("paises.json");
+  const data = await response.json();
+  return data;
+}
+async function fetchTarifas(tarifaActual) {
   const res = await fetch(`https://open.er-api.com/v6/latest/${tarifaActual}`);
   const data = await res.json();
   const tarifas = data.rates;
+  return tarifas;
+}
+
+async function actualizarMontos(tarifaSeleccionada, montoInput) {
+  const tarifas = await fetchTarifas(tarifaSeleccionada);
 
   for (const tarifa in tarifas) {
     const celdasMonto = document.getElementsByClassName(`${tarifa}`);
     for (const celda of celdasMonto) {
-      celda.textContent =Math.min
-        (tarifas[tarifa] * (montoInput || 1) ).toFixed(2) + "$";
+      const simbolo = celda.classList[1];
+      const monto = Math.min(tarifas[tarifa] * (montoInput || 1)).toFixed(2);
+      celda.textContent = `${monto}  ${simbolo}`;
     }
   }
 }
 
 function renderizarPaises(data) {
-  for (const { currencies, translations, languages, flags, name } of data) {
-    if (currencies) {
-      const divisa = Object.keys(currencies)[0];
-      const moneda = currencies[Object.keys(currencies)[0]].name;
-      const pais = translations.spa.common;
-      const lenguaje = Object.values(languages)[0];
-      const srcBandera = flags.png;
-      const altBandera = flags.alt;
+  for (const { currencies, translations, languages, flags } of data) {
+    const divisa = Object.keys(currencies)[1] || Object.keys(currencies)[0];
+    const moneda = currencies[Object.keys(currencies)[0]].name;
+    const pais = translations.spa.common;
+    const lenguaje = Object.values(languages)[0];
+    const srcBandera = flags.png;
+    const altBandera = flags.alt;
 
-      const filasClonadas = template.cloneNode(true);
-      const celdasClonadas = filasClonadas.querySelectorAll("td");
+    const filasClonadas = template.cloneNode(true);
+    const celdasClonadas = filasClonadas.querySelectorAll("td");
 
-      celdasClonadas[0].classList.add(divisa);
-      celdasClonadas[1].textContent = divisa;
-      celdasClonadas[2].textContent = moneda;
-      celdasClonadas[3].textContent = pais;
-      celdasClonadas[4].textContent = lenguaje;
-      celdasClonadas[5].querySelector("img").src = srcBandera;
-      celdasClonadas[5].querySelector("img").alt = altBandera;
-      celdasClonadas[6].textContent = "ver mas";
-      fragment.appendChild(filasClonadas);
-    } else {
-      //console.log(name.common, "No tiene currencies"); //Paises sin moneda
-    }
+    celdasClonadas[0].classList.replace("simbolo", filtrarSimbolo(currencies));
+    celdasClonadas[0].classList.add(divisa);
+    celdasClonadas[1].textContent = divisa;
+
+    celdasClonadas[2].textContent = moneda;
+    celdasClonadas[3].textContent = pais;
+    celdasClonadas[4].textContent = lenguaje;
+    celdasClonadas[5].querySelector("img").src = srcBandera;
+    celdasClonadas[5].querySelector("img").alt = altBandera;
+    celdasClonadas[6].textContent = "ver mas";
+    fragment.appendChild(filasClonadas);
   }
   tbody.appendChild(fragment);
 }
 
-async function cargarDatos() {
-  const response = await fetch("paises.json");
-  const data = await response.json();
-  renderizarPaises(data);
-  agregarDatosASelect(data);
-  actualizarMontos(tarifaActual);
-}
-
-function agregarDatosASelect(data) {
+function renderizarOpcionesDePaisesSeleccionables(data) {
   data.sort((a, b) => {
     if (a.name.common > b.name.common) {
       return 1;
@@ -81,13 +102,23 @@ function agregarDatosASelect(data) {
     }
   });
 
-  for (const { currencies, translations } of data)
-    if (currencies) {
-      const divisa = Object.keys(currencies)[0];
-      const pais = translations.spa.common;
-      const opcion = document.createElement("option");
-      opcion.value = divisa;
-      opcion.textContent = pais;
-      seleccionarPais.appendChild(opcion);
-    }
+  for (const { currencies, translations } of data) {
+    const divisa = Object.keys(currencies)[1] || Object.keys(currencies)[0];
+    const pais = translations.spa.common;
+    const opcion = document.createElement("option");
+    const moneda = currencies[Object.keys(currencies)[0]].name;
+    const simboloDivisa = filtrarSimbolo(currencies).replace(/\s/g, "");
+    opcion.value = divisa;
+    opcion.textContent = pais;
+    opcion.classList.add(simboloDivisa);
+    opcion.setAttribute("id", moneda);
+    seleccionarPais.appendChild(opcion);
+  }
+}
+function filtrarSimbolo(currencies) {
+  if (currencies[Object.keys(currencies)]?.symbol) {
+    return currencies[Object.keys(currencies)].symbol.replace(/\s/g, "");
+  } else {
+    return "$";
+  }
 }
